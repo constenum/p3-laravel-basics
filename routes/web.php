3b1e02eb3d05aa1876eb71c9de726439f9aed85c
Mark Miller <constenum@gmail.com>
2016-10-15 15:15:13 -0400

    Add main page route and get and post routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# Index for website homepage
+Route::get('/', 'HomeController@index')->name('home.index');
+
+# Index for Lorem Ipsum generator
+Route::get('/lorem', 'LoremController@index')->name('lorem.index');
+
+# Process Lorem Ipsum generator form
+Route::post('/lorem', 'LoremController@store')->name('lorem.store');
+
+# Index for Lorem Ipsum generator
+Route::get('/user', 'UserController@index')->name('user.index');
+
+# Process Lorem Ipsum generator form
+Route::post('/user', 'UserController@store')->name('user.store');
+
+# Index for Lorem Ipsum generator
+Route::get('/color', 'ColorController@index')->name('color.index');
+
+# Process Lorem Ipsum generator form
+Route::post('/color', 'ColorController@store')->name('color.store');
+
+# Index for Lorem Ipsum generator
+Route::get('/xkcd', 'XkcdController@index')->name('xkcd.index');
+
+# Process Lorem Ipsum generator form
+Route::post('/xkcd', 'XkcdController@store')->name('xkcd.store');
