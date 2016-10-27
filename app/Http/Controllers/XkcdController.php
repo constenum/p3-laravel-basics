5ce4e40ed0e23b08a7c67e26179f0db575eb2aa0
Mark Miller <constenum@gmail.com>
2016-10-27 16:11:36 -0400

    Add xkcd password controller and view

--- app/Http/Controllers/XkcdController.php
+++ app/Http/Controllers/XkcdController.php
@@ -15,17 +15,69 @@ class XkcdController extends Controller
      */
     public function index()
     {
-        return 'TODO: create xkcd index page';
+        return view('xkcd.index')->with('results', session('results'));
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Show a newly created resource.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function show(Request $request)
     {
-        return 'TODO: create xkcd generator logic';
+        # Form Data
+        $number = $request->input('numberOfWords');
+        $digit = $request->input('numberOfDigits');
+        $symbol = $request->input('numberOfSymbols');
+
+        # Validation
+        $this -> validate($request, [
+                'numberOfWords' => 'required|integer|min:4|max:8',
+                'digitsCheckbox' => 'required_with:numberOfDigits',
+                'numberOfDigits' => 'required_with:digitsCheckbox|integer|min:1|max:9',
+                'symbolsCheckbox' => 'required_with:numberOfSymbols',
+                'numberOfSymbols' => 'required_with:symbolsCheckbox|integer|min:1|max:3',
+            ]);
+
+        # Logic
+        $word_list = [];
+        $file = file_get_contents('./csv/word_list.csv');
+        $lines = explode(PHP_EOL, $file);
+        foreach ($lines as $line) {
+            $word_list = str_getcsv($line);
+        }
+
+        $numbers_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+        $symbols_list = ['(', ')', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '-', '+', '=', '|', '\\', '{', '}', '[', ']', ':', ';'];
+
+        $rand_words = '';
+
+        for ($i = 1; $i <= $number; $i++) {
+	    	$rand_keys = array_rand($word_list, 1);
+	    	$rand_words .= $word_list[$rand_keys]. ' ';
+        }
+
+        $results = rtrim($rand_words);
+
+        if ($request->has('digitsCheckbox')) {
+			for ($i = 1; $i <= $digit; $i++) {
+		    	$rand_keys = array_rand($numbers_list, 1);
+		    	$results .= $numbers_list[$rand_keys];
+			}
+		}
+
+		if ($request->has('symbolsCheckbox')) {
+			for ($i = 1; $i <= $symbol; $i++) {
+		    	$rand_keys = array_rand($symbols_list, 1);
+		    	$results .= $symbols_list[$rand_keys];
+			}
+		}
+
+		if ($request->has('capitalize')) {
+			$results = ucwords($results);
+		}
+
+        return \Redirect::to('xkcd')->with('results', $results);
     }
 }
---
+++ resources/views/xkcd/index.blade.php
@@ -0,0 +1,92 @@
+@extends("layouts.master")
+
+@section("title", "xkcd Password Generator")
+
+{{-- Page content --}}
+@section("content")
+<div class="container">
+
+    <img src="/img/web_developer_tools.jpg" class="img-responsive home-img" alt="Developer Tools">
+    <h1>xkcd Password Generator</h1>
+    <div class="well">
+        <p>
+            To generate a xkcd password, enter the number of words, between 4 and 8, to be included
+            in the password. Additionally, select any options. The results will be displayed below
+            the form.
+        </p>
+    </div>
+
+    <form method="POST" action="/xkcd">
+        {{ csrf_field() }}
+
+        <fieldset class="border">
+            <legend class="border">Number of xkcd Password Words</legend>
+            <div class="form-group">
+            <input type="number" class="form-control" id="number" name="numberOfWords"
+                placeholder="Enter number of words">
+            </div>
+        </fieldset>
+
+        <fieldset class="border">
+            <legend class="border">xkcd Password Options</legend>
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <label><input type="checkbox" name="digitsCheckbox" id="digits" value="digits"> Add digit(s) to the last word</label>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="form-group">
+                        <label>How many digits (between 1 and 9) </label>
+                        <input type="number" name="numberOfDigits">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="form-group">
+                        <label><input type="checkbox" name="symbolsCheckbox" id="symbols" value="symbols"> Add digit(s) to the last word</label>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div class="form-group">
+                        <label>How many symbols (between 1 and 3) </label>
+                        <input type="number" name="numberOfSymbols">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group">
+                        <label><input type="checkbox" name="capitalize" id="capitalize"> Capitalize the first letter of each word</label>
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+
+        @if(count($errors) > 0)
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <button type="submit" class="btn btn-primary btn-lg btn-marg">Generate xkcd Password</button>
+    </form>
+    <br>
+
+    @if (isset($results))
+        <h3>Lorem Ipsum</h3>
+        <p>
+            <div class="well well-lg">
+                {!! $results !!}
+            </div>
+        </p>
+    @endif
+</div>
+@endsection
